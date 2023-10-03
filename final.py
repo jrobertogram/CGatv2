cdec9def9886c9529f1c6f8885af63472d3eb766
luckas-ferreira <luckas0ferreira321@gmail.com>
2023-10-02 22:08:52 -0300

    textura no piso e bola

--- final.py
+++ final.py
@@ -54,9 +54,9 @@ def pisotextura():
     glPushMatrix()
     glTranslate( 0.0, 0.0, 0.0)  #Transtacao do objeto
     # Textured 
-    #tex = load_texture('xadrez.jpg')  
-   # glEnable(GL_TEXTURE_2D)
-    #glBindTexture(GL_TEXTURE_2D, tex)
+    tex = load_texture('ceramica.jpg')  
+    glEnable(GL_TEXTURE_2D)
+    glBindTexture(GL_TEXTURE_2D, tex)
     
     glBegin(GL_POLYGON)
     #gluCylinder(textura, largura da base, largura do topo, altura, resolucao , resolucao)
@@ -132,7 +132,6 @@ def mao():
 def desenho():
     #eixos()
     
-    
     pisotextura()
     mao()
     # OBJ BOLA
@@ -141,8 +140,21 @@ def desenho():
     # Variaveis globais que vão estar movimentando a bola
     glTranslate(bol2, bol1, -0.4)
     glScale(bolscale,1,bolscale)
-    glutSolidSphere(0.2, 20, 20)
+    
+    # Textured thing 
+    tex = read_texture('bola_Tenis.jpg')
+    qobj = gluNewQuadric()
+    gluQuadricTexture(qobj, GL_TRUE)
+    glEnable(GL_TEXTURE_2D)
+    glBindTexture(GL_TEXTURE_2D, tex)
+    
+    gluSphere(qobj, 0.2, 20, 20) # Use gluSphere instead of glutSolidSphere
+    gluDeleteQuadric(qobj)
+    
+    glDisable(GL_TEXTURE_2D)
+
     glPopMatrix()
+
 
 def load_texture(filename):
     img = Image.open(filename)
@@ -250,6 +262,25 @@ def tela():
     desenho()
     glFlush()
     glutSwapBuffers()
+
+
+def read_texture(filename):
+    img = Image.open(filename)
+    img_data = numpy.array(list(img.getdata()), numpy.int8)
+    textID = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D, textID)
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.size[0], img.size[1], 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
+    
+    return textID
+
 
 # Aqui vai acontecer a mudanção (quando chegar nas coordenadas predefinidas, ou quando colidir
 def update(value):
